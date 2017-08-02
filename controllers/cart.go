b9bf6c46c999584417e7d5ce9997f310727868f8
leiliang <liang.lei@mtime.com>
2017-08-02 17:56:07 +0800

    recommit

---
+++ controllers/cart.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"ChiMu/basic"
+)
+
+type CartAddController struct {
+	basic.BasicController
+}
+
+func (c *CartAddController) Get() {
+	// pid, _ := c.GetInt("pid")
+	// uid, _ := c.GetInt("uid")
+	// count, _ := c.GetInt("count")
+
+	// cart := services.GetCart(pid, uid)
+	// if cart == nil {
+
+	// }
+}
